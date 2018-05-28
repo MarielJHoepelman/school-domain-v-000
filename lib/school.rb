3ef4921074a43da9ea7a823ada1625980db97e9e
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-05-28 15:36:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,19 @@
 class School
-  def initialize
+
+  ROSTER = []
+  
+  def initialize(students)
+    @students = students
+    add_student
   end
+
+  def add_student
+      if !ROSTER.include?(@students)
+        ROSTER << students
+      end
+    end
+    
+
+
+  
 end
