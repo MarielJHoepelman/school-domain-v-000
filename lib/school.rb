53fa7a7eeae95798b0d40d2861e32ca4b33c7203
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-05-28 16:02:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,10 +9,11 @@ class School
   end
 
   def add_student(name, grade)
+    binding.pry
     if @roster[grade]
       @roster[grade] << name
     else
       @roster[grade] = [name]
-    end 
+    end
   end
 end
